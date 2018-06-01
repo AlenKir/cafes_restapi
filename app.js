65a0ba12cfa3ebb98d2e4e9a14a83b1a5ee88c34
AlenMuseJ <alen.muse@yandex.ru>
2018-06-01 12:53:10 +0300

    find by id and by name

--- app.js
+++ app.js
@@ -18,14 +18,46 @@ const connection = mysql.createConnection(db)
 connection.connect(function(err) {
   if (err) throw err;
   
-  
 app.get('/cafes', (req, res) => {
-connection.query("SELECT * FROM cafe", function (err, rows, result) {
+	const q = "SELECT * FROM cafe";
+connection.query(q, function (err, rows, result) {
     if (err) { 
         res.send({ 'error': 'An error has occurred' }); 
       }
 	  else{
     res.send(rows);
+	  console.log(rows);
+	  }
+	});
+});
+
+app.get('/cafebyid/:id', (req, res) => {
+	const q = "SELECT * FROM cafe WHERE cafe_id = ?";
+	const cafeId = req.params.id;
+	console.log("request cafe with id = " + cafeId);
+	connection.query(q, [cafeId], function (err, rows, result) {
+    if (err) { 
+		res.send(err)
+        //res.send({ 'error': 'An error has occurred' }); 
+      }
+	  else{
+		res.send(rows);
+	  console.log(rows);
+	  }
+	});
+});
+
+app.get('/cafebyname/:name', (req, res) => {
+	const q = "SELECT * FROM cafe WHERE cafe_name = ?";
+	const cafeName = req.params.name;
+	console.log("request cafe with cafe_name = " + cafeName);
+	connection.query(q, [cafeName], function (err, rows, result) {
+    if (err) { 
+		res.send(err)
+        //res.send({ 'error': 'An error has occurred' }); 
+      }
+	  else{
+		res.send(rows);
 	  console.log(rows);
 	  }
 	});
